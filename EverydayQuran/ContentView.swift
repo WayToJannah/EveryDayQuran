97df82fa65d8121cdd6d59dbf4766b344a808fb8
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-04-26 18:09:10 +0600

    Initial Commit

---
+++ EverydayQuran/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EverydayQuran
+//
+//  Created by Shahriar Nasim Nafi on 26/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EverydayQuran/EverydayQuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverydayQuranApp.swift
+//  EverydayQuran
+//
+//  Created by Shahriar Nasim Nafi on 26/4/21.
+//
+
+import SwiftUI
+
+@main
+struct EverydayQuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
